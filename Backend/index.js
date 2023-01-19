527147bc39e08574141ad208dd731da578c89d55
Rati3010 <mratikanta1997@gmail.com>
2023-01-19 13:22:36 +0530

    pushed in diff repo

---
+++ Backend/index.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const { connection } = require("./config/db.js");
+const { UserModel } = require("./models/userModel.js");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const salt = parseInt(process.env.salt);
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(express.json());
+app.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+  const checkEmail = await UserModel.find({ email });
+  if (checkEmail.length > 0) {
+    res.send({ msg: "Email already exist" });
+  } else {
+    try {
+      bcrypt.hash(password, salt, async (err, hash) => {
+        if (err) {
+          console.log(err);
+          res.send({ msg: "Registration Failed" });
+        } else {
+          const new_user = new UserModel({ email, password: hash });
+          await new_user.save();
+          res.send({ msg: "Registration Success" });
+        }
+      });
+    } catch (error) {
+      console.log(error);
+      res.send({ msg: "Registration Failed" });
+    }
+  }
+});
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const checkEmail = await UserModel.find({ email });
+    if (checkEmail.length > 0) {
+      const hashed = checkEmail[0].password;
+      bcrypt.compare(password, hashed, (err, result) => {
+        if (err) {
+          console.log(err);
+          res.send({ err: "Login failed" });
+        }
+        if (result) {
+          const token = jwt.sign({ id: checkEmail[0]._id }, process.env.key);
+          res.send({ msg: "Login Successfull", token: token });
+        } else {
+          res.send({ msg: "your passwor is not correct" });
+        }
+      });
+    } else {
+      res.send({ msg: "Login First" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.send({ msg: "something went wrong" });
+  }
+});
+app.listen(process.env.port || 3001, async () => {
+  try {
+    await connection;
+    console.log("connected to database");
+  } catch (error) {
+    console.log(error);
+    console.log("Database is not responding");
+  }
+});
